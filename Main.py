31f6051087b0d86a3df25633e9ecd5f24d646938
Elias Nieres-tavernier <elias.nieres-tavernier@edu.devinci.fr>
2025-01-14 15:04:30 +0100

    Projet todolist

---
+++ Main.py
@@ -0,0 +1,60 @@
+import os
+
+
+
+inp=5
+comp=0
+tasks=[]
+while(inp!=4):
+    
+    print("Welcome to your to todolist : ")
+    a=1
+    for i in tasks:
+        print(f"{a}. {i}")
+        a+=1
+    print(f"You have completed {comp} tasks")
+        
+
+    print("What do you want to do ? \n1.Add a new task \n2.Delete a task \n3.Complete a task \nTo leave enter any other keys")
+    input1=input()
+    if(input1=="1"):
+        print("Enter the task to add")
+        tasks.append(input())
+        os.system('cls')
+    elif(input1=="2"):
+        if(len(tasks)!=0):
+            print("Choose the task to delete")
+            entry=input()
+            if(entry.isnumeric!=False):
+                entry=int(entry)
+                if(entry>len(tasks)):
+                    os.system('cls')
+                    print("impossible to remove")
+                else:
+                    tasks.pop(entry-1)
+            else:
+                os.system('cls')
+                print("impossible to remove")
+        else:
+            os.system('cls')
+            print("The list is empty you can't remove anything")
+    elif(input1=="3"):
+        if(len(tasks)!=0):
+            print("Which task have you completed")
+            entry=input()
+            if(entry.isnumeric!=False):
+                entry=int(entry)
+                if(entry>len(tasks)):
+                    os.system('cls')
+                    print("the task doesnt exist")
+                else:
+                    tasks.pop(entry-1)
+                    comp=comp+1
+            else:
+                os.system('cls')
+                print("the task doesnt exist")
+        else:
+            os.system('cls')
+            print("The list is empty there is nothing to complete")
+    else:
+        inp=4
---
+++ setup.py
@@ -0,0 +1,9 @@
+ #!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='ToDoList-EliasNieres-Tavernier-CDOF3',
+version='1.0',
+author='Elias Nieres-Tavernier',
+license='MIT',
+long_description=open('README.md').read(),
+)
